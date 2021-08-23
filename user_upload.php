8bd9ee3fd457aa2544ca3a6cb624fc70d304f9b3
Purshotam Rajthala <purshotam@sydneydigitalgroup.com.au>
2021-08-24 09:48:09 +1000

    Added user_upload file and done displaying help

---
+++ user_upload.php
@@ -0,0 +1,30 @@
+<?php
+
+
+if (in_array("--help", $argv)) {
+     displayOptions();
+     die();
+}
+
+if(!isset($argv[1])){
+      echo "Directive is required";
+      displayOptions();
+      die(); 
+};
+
+function displayOptions()
+{
+    echo "
+ --file [csv file name] – this is the name of the CSV to be parsed
+ --create_table – this will cause the MySQL users table to be built (and no further action will be taken)
+ --dry_run – this will be used with the --file directive in case we want to run the
+            script but not insert into the DB. All other functions will be executed, but the
+             database won't be altered
+ -u – MySQL username
+ -p – MySQL password
+ -h – MySQL host
+ --help – which will output the above list of directives with details.
+    ";
+}
+
+?>
