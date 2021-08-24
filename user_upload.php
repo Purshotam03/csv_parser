296146e0922917b8d778444324976d20dce5f7c4
Purshotam Rajthala <purshotam@sydneydigitalgroup.com.au>
2021-08-25 09:14:01 +1000

    added csv file and worked on input parameter validation

--- user_upload.php
+++ user_upload.php
@@ -1,16 +1,102 @@
 <?php
 
+// Variables to get options from users
+$fileCSV = "";
+$fileIncluded = false;
+$createTable = false;
+$dryRun = false;
+$username = "";
+$password = "";
+$hostName = "";
 
+//If the user select help
 if (in_array("--help", $argv)) {
-     displayOptions();
-     die();
+    displayOptions();
+    die();
 }
 
-if(!isset($argv[1])){
-      echo "Directive is required";
-      displayOptions();
-      die(); 
-};
+//If the user run program with out parameter
+if (!isset($argv[1])) {
+    echo "Directive is required";
+    displayOptions();
+    die();
+}
+
+//Checking csv file
+if (in_array("--file", $argv)) {
+    $indexOfParameterFile = array_search("--file", $argv);
+    if (isset($argv[$indexOfParameterFile + 1])) {
+        $fileCSV = $argv[$indexOfParameterFile + 1];
+        if (!(file_exists($fileCSV) && is_file($fileCSV))) {
+            echo("Invalid file name provided\n");
+            die();
+        }
+    } else {
+        echo "Provide File Name";
+        die();
+    }
+}
+
+//Checking for create table or dryRun
+if (in_array("--create_table", $argv)) {
+    $createTable = true;
+}
+
+if (in_array("--dry_run", $argv)) {
+    $dryRun = true;
+}
+
+//Checking if the user uses both create table and dry run
+if($createTable&&$dryRun){
+    echo "Please select --create_table or __dry_run";
+    displayOptions();
+    die();
+}
+
+//Checking for username,password and host from user and storing in variables
+if (in_array("-u", $argv)) {
+    $index = array_search("-u", $argv);
+    if (isset($argv[$index + 1])) {
+        $username = $argv[$index + 1];
+    }
+}
+
+if (in_array("-p", $argv)) {
+    $index = array_search("-p", $argv);
+    if (isset($argv[$index + 1])) {
+        $password = $argv[$index + 1];
+    }
+}
+
+if (in_array("-h", $argv)) {
+    $index = array_search("-h", $argv);
+    if (isset($argv[$index + 1])) {
+        $hostName = $argv[$index + 1];
+    }
+}
+
+//if the user selects create_table option only
+if($createTable&($username=="" || $hostName=="")){
+    echo "The parameters Username and Host are required. Please try again.\n";
+    displayOptions();
+    die();
+}
+
+//if the user selects dry_run option only
+if($dryRun){
+    if($fileCSV==""){
+        echo "Please use --file with file name";
+        die();
+    }
+}
+
+//if the user input file name with out username and host
+if(!($fileCSV=="")&($username=="" || $hostName=="")){
+    echo "The parameters Username and Host are required. Please try again.\n";
+    displayOptions();
+    die();
+}
+
 
 function displayOptions()
 {
